115a97b878c4ecb761bf4c0f1529bce0f83b50cb
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-08-10 15:04:30 +0530

    feat(js): create functions for basic operators
    Created functions for addition, subtraction, multiplication, and division.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(3, 5));
+console.log(subtract(2, 5));
+console.log(multiply(67, 8));
+console.log(divide(81, 9));
