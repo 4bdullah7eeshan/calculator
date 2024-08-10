2a3a0c5fc9e37e8ac0c0f6af196d28d3283785e8
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-08-10 16:07:25 +0530

    feat: create a basic calculator interface
    - No JS here yet
    - A place for display screen
    - A place for clear and backspace button
    - Buttons for all digits
    - Buttons for a decimal point(.)
    - Buttons for basic arithmetic operators and equals symbol

--- js/script.js
+++ js/script.js
@@ -28,3 +28,37 @@ function operate(a, b, s) {
 
 let firstNumber, secondNumber, operator;
 
+const calculator = document.querySelector("#calculator");
+
+const display = document.createElement("button");
+display.setAttribute("style", `height: ${100 / 6}%; width: 100%; outline: 1px solid black`);
+display.textContent = "1234567890";
+calculator.appendChild(display);
+
+const clearButton = document.createElement("button");
+clearButton.setAttribute("style", `height: ${100 / 6}%; width: 50%; outline: 1px solid black`);
+clearButton.textContent = "A/C";
+calculator.appendChild(clearButton);
+
+
+const backspaceButton = document.createElement("button");
+backspaceButton.setAttribute("style", `height: ${100 / 6}%; width: 50%; outline: 1px solid black`);
+backspaceButton.textContent = "Backspace"
+calculator.appendChild(backspaceButton);
+
+const buttonTexts = [
+    "7", "8", "9", "/",
+    "4", "5", "6", "*",
+    "1", "2", "3", "-",
+    "0", ".", "=", "+"
+];
+
+for (let i = 0; i < 16; i++) {
+    const btn = document.createElement("button");
+    btn.setAttribute("style", `height: ${100 / 6}%; width: 25%; outline: 1px solid black;`); 
+    btn.textContent = buttonTexts[i];
+    btn.addEventListener("click", function() {
+        hover(btn);
+    });
+    calculator.appendChild(btn);
+}
